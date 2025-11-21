710b18dd11fb643d6645af3d45845ffc20883e6f
Eray Demircioglu <c.eray@hotmail.com>
2025-11-21 18:35:51 +0300

    Ecommerce Frontend

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://ecomerce-case-backend.onrender.com";
 
 type ApiError = {
   message?: string;
