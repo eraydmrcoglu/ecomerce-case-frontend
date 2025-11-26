3b17b316d0881a011b3e482ce144f0fb755a7d04
Eray Demircioglu <c.eray@hotmail.com>
2025-11-26 14:19:33 +0300

    Ecommerce Frontend

--- next.config.ts
+++ next.config.ts
@@ -21,10 +21,6 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "images.pexels.com",
-      },
-      {
-        protocol: "https",
         hostname: "via.placeholder.com",
       },
     ],
